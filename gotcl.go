88c468fbdbdda4284b08e75a246541e8603db259
Niels Widger <niels.widger@gmail.com>
2013-07-21 08:45:30 -0400

    * gotcl.go: Built-in 'set' command works for getting/setting local   variables

--- gotcl.go
+++ gotcl.go
@@ -1,5 +1,5 @@
 // Niels Widger
-// Time-stamp: <20 Jul 2013 at 21:04:52 by nwidger on macros.local>
+// Time-stamp: <21 Jul 2013 at 08:44:16 by nwidger on macros.local>
 
 package main
 
@@ -13,50 +13,43 @@ import (
 	"strings"
 )
 
-var COMMAND_SEP regexp.Regexp = *regexp.MustCompile("[;\n]")
-
-var WORD_SEP regexp.Regexp = *regexp.MustCompile("[ \t\f]")
-
-var ARG_EXP regexp.Regexp = *regexp.MustCompile("{\\*}[^ \t\f]")
-
-var NAME regexp.Regexp = *regexp.MustCompile("([a-zA-Z0-9_]|::)+")
-
-// var BACKSLASH_SUB regexp.Regexp = *regexp.MustCompile("\\([abfnrtv\\]|[0-9]{1-3}|(x([0-9a-fA-F])*)|(u([0-9a-fA-F]){1-4}))")
-
 type Word string
 type Value string
 
-type Command struct {
-	name string
-	num_args int
-	args []string
-
-	body string
-	native_body (func (*Interp, []string) string)
-}
+// FRAME
 
 type Frame struct {
 	level int
-	vars map[string]*Value
-}
-
-type Stack struct {
-	level_list *list.List
-	level_map map[int]*Frame
+	vars map[string]Value
 }
 
 func NewFrame() *Frame {
-	return &Frame{ level: 0, vars: make(map[string]*Value) }
+	return &Frame{ level: 0, vars: make(map[string]Value) }
 }
 
-func (frame *Frame) GetValue(varName string) (ok bool, value *Value) {
+func (frame *Frame) GetValue(varName string) (ok bool, value Value) {
 	value, ok = frame.vars[varName]
 	return
 }
 
-func (frame *Frame) SetValue(varName string, value *Value) (ok bool) {
+func (frame *Frame) SetValue(varName string, value Value) (ok bool) {
 	frame.vars[varName] = value
 	return true
+}
+
+func (frame *Frame) GetLevel() int {
+	return frame.level
+}
+
+func (frame *Frame) GetParentLevel() int {
+	return frame.level - 1
+}
+
+// STACK
+
+type Stack struct {
+	level_list *list.List
+	level_map map[int]*Frame
 }
 
 func (stack *Stack) PushFrame(frame *Frame) {
@@ -100,6 +93,8 @@ func (stack *Stack) PopFrame() {
 	}
 }
 
+// NAMESPACE
+
 type Namespace struct {
 	name string
 	commands map[string]Command
@@ -129,6 +124,38 @@ func (ns *Namespace) FindCommand(name string, words *list.List) (cmd Command, er
 	return cmd, error
 }
 
+// COMMAND
+
+type Command struct {
+	name string
+	num_args int
+	args []string
+
+	body string
+	native_body (func (*Interp, []string) string)
+}
+
+func (cmd *Command) BadArgsMessage() string {
+	var err = fmt.Sprintf("wrong # args: should be \"%v", cmd.name)
+	for _, arg := range cmd.args { err += fmt.Sprintf(" %v", arg) }
+	err += "\""
+	return err
+}
+
+func (cmd *Command) ValidateArgs(words *list.List) (ok bool, err error) {
+	ok = true
+	err = nil
+
+	if cmd.num_args != -1 && cmd.num_args != words.Len() {
+		ok = false
+		err = errors.New(cmd.BadArgsMessage())
+	}
+
+	return
+}
+
+// INTERP
+
 type Interp struct {
 	stack Stack
 	namespaces map[string]Namespace
@@ -139,7 +166,6 @@ func NewInterp() *Interp {
 	interp := &Interp{ Stack{ level_map: make(map[int]*Frame), level_list: list.New() }, make(map[string]Namespace) }
 	interp.namespaces["::"] = NewNamespace("::")
 	interp.AddBuiltinCommands()
-	interp.stack.PushFrame(NewFrame())
 	return interp
 }
 
@@ -187,44 +213,32 @@ func (interp *Interp) AddBuiltinCommands() {
 	}})
 
 	interp.AddCommand(ns_name, Command{ "set", -1, []string{ "varName", "value" }, "", func (interp *Interp, args []string) string {
-		frame := interp.stack.PeekFrame()
+		var ok bool
+		var value Value
 
 		varName := args[0]
+		level := interp.stack.PeekFrame().GetParentLevel()
+		frame, error := interp.stack.GetFrame(level)
 
-		if len(args) == 2 {
-			value := Value(args[1])
-			frame.SetValue(varName, &value)
-			return string(value)
-		}
-
-		ok, value := frame.GetValue(varName)
-
-		if !ok {
-			fmt.Printf("can't read \"%s\": no such variable\n", varName)
+		if error != nil {
+			fmt.Println(error)
 			os.Exit(1)
 		}
 
-		return string(*value)
+		if len(args) == 2 {
+			value = Value(args[1])
+			frame.SetValue(varName, value)
+		} else {
+			ok, value = frame.GetValue(varName)
+
+			if !ok {
+				fmt.Printf("can't read \"%s\": no such variable\n", varName)
+				os.Exit(1)
+			}
+		}
+
+		return string(value)
 	}})
-}
-
-func (cmd *Command) BadArgsMessage() string {
-	var err = fmt.Sprintf("wrong # args: should be \"%v", cmd.name)
-	for _, arg := range cmd.args { err += fmt.Sprintf(" %v", arg) }
-	err += "\""
-	return err
-}
-
-func (cmd *Command) ValidateArgs(words *list.List) (ok bool, err error) {
-	ok = true
-	err = nil
-
-	if cmd.num_args != -1 && cmd.num_args != words.Len() {
-		ok = false
-		err = errors.New(cmd.BadArgsMessage())
-	}
-
-	return
 }
 
 func (interp *Interp) FindCommand(name string, words *list.List) (cmd Command, error error) {
@@ -238,10 +252,12 @@ func (interp *Interp) FindCommand(name string, words *list.List) (cmd Command, e
 		return
 	}
 
-	return cmd, error
+	return
 }
 
 func (interp *Interp) ParseDoubleQuoteWord(script string) (ok bool, word string, remainder string) {
+	script = interp.SkipWhiteSpace(script)
+
 	ok = false
 	remainder = script
 
@@ -251,12 +267,14 @@ func (interp *Interp) ParseDoubleQuoteWord(script string) (ok bool, word string,
 
 	ok = true
 	word = script[loc[0]+1:loc[1]-1]
-	remainder = string(script[loc[1]:])
+	remainder = interp.SkipWhiteSpace(string(script[loc[1]:]))
 
 	return
 }
 
 func (interp *Interp) ParseBraceWord(script string) (ok bool, word string, remainder string) {
+	script = interp.SkipWhiteSpace(script)
+
 	ok = false
 	remainder = script
 
@@ -266,7 +284,7 @@ func (interp *Interp) ParseBraceWord(script string) (ok bool, word string, remai
 
 	ok = true
 	word = script[loc[0]+1:loc[1]-1]
-	remainder = string(script[loc[1]:])
+	remainder = interp.SkipWhiteSpace(string(script[loc[1]:]))
 
 	return
 }
@@ -285,13 +303,13 @@ func (interp *Interp) ParseWord(script string) (ok bool, word string, remainder 
 	ok = false
 	remainder = script
 
-	loc := regexp.MustCompile("^[^ \t\v\r\f\n]+").FindStringIndex(script); if loc == nil {
+	loc := regexp.MustCompile("^[^ \t\v\f\r\n;]+").FindStringIndex(script); if loc == nil {
 		return
 	}
 
 	ok = true
 	word = script[loc[0]:loc[1]]
-	remainder = string(script[loc[1]:])
+	remainder = interp.SkipWhiteSpace(string(script[loc[1]:]))
 
 	return
 }
@@ -303,16 +321,16 @@ func (interp *Interp) ParseWords(script string) (ok bool, words *list.List, rema
 	words = list.New()
 
 	for len(remainder) != 0 {
+		if string(remainder[0]) == "\n" || string(remainder[0]) == ";" {
+			break
+		}
+
 		ok, word, remainder = interp.ParseWord(remainder); if !ok {
 			fmt.Printf("error parsing next word from script \"%s\"\n", remainder)
 			os.Exit(1)
 		}
 
 		words.PushBack(word)
-
-		if len(remainder) > 0 && (string(remainder[0]) == "\n" || string(remainder[0]) == ";") {
-			break
-		}
 	}
 
 	return true, words, remainder
@@ -329,20 +347,28 @@ func (interp *Interp) SkipWhiteSpace(script string) (remainder string) {
 	return
 }
 
+func (interp *Interp) wordListToArray(words *list.List) []string {
+	i := 0
+	swords := make([]string, words.Len())
+
+	for e := words.Front(); e != nil; e = e.Next() {
+		swords[i] = e.Value.(string)
+		i++
+	}
+
+	return swords
+}
+
 func (interp *Interp) Eval(script string) string {
-	var ok bool
 	var words *list.List
 	var error error
 	var cmd Command
 
 	retval := ""
+	interp.stack.PushFrame(NewFrame())
 
 	for len(script) != 0 {
-		ok, words, script = interp.ParseWords(script)
-
-		if !ok {
-
-		}
+		_, words, script = interp.ParseWords(script)
 
 		if len(script) > 0 && (string(script[0]) == "\n" || string(script[0]) == ";") {
 			script = script[1:]
@@ -364,19 +390,15 @@ func (interp *Interp) Eval(script string) string {
 			if cmd.native_body == nil {
 				retval = interp.Eval(cmd.body)
 			} else {
-				i := 0
-				swords := make([]string, words.Len())
-
-				for e := words.Front(); e != nil; e = e.Next() {
-					swords[i] = e.Value.(string)
-					i++
-				}
-
-				retval = cmd.native_body(interp, swords)
+				interp.stack.PushFrame(NewFrame())
+				retval = cmd.native_body(interp, interp.wordListToArray(words))
+				interp.stack.PopFrame()
 			}
 
 		}
 	}
+
+	interp.stack.PopFrame()
 
 	return retval
 }
